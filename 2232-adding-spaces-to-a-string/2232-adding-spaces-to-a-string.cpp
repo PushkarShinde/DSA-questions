03c34d6648aba70566519864e182e7f2244fbbd4
Pushkar Shinde <pushkarshinde24@gmail.com>
2024-12-03 23:21:44 +0530

    Time: 16 ms (79.82%), Space: 85.5 MB (18.48%) - LeetHub

---
+++ 2232-adding-spaces-to-a-string/2232-adding-spaces-to-a-string.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    string addSpaces(string s, vector<int>& spaces) {
+        string result;
+        int sp=0;
+        for (int i=0; i<s.length();i++){
+            if(sp< spaces.size() && i==spaces[sp]){
+                result+=' ';
+                sp++;
+            }
+            result+=s[i];
+        }
+        return result;
+    }
+};
