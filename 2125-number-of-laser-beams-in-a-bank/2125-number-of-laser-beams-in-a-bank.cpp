64058c5679fd59535512668234c8e6b87ddf29d8
Pushkar Shinde <pushkarshinde24@gmail.com>
2024-12-27 00:16:19 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 2125-number-of-laser-beams-in-a-bank/2125-number-of-laser-beams-in-a-bank.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int numberOfBeams(vector<string>& bank) {
+        int count=0;
+        vector<int> weight;
+        for(int i = 0; i < bank.size(); i++) {
+            for(auto j: bank[i]) {
+                if(j == '1') {
+                    count++;
+                }
+            }
+            if(count != 0) {
+                weight.push_back(count);
+            }
+            count = 0;  // Reset count for the next row
+        }
+        int c=0;
+        for(int i=0;i<weight.size()-1;i++){
+            c+=weight[i]*weight[i+1];
+        }
+        return c;
+    }
+};
