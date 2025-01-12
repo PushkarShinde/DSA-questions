d676d661dc34e5659082df46200fcf453b810679
Pushkar Shinde <pushkarshinde24@gmail.com>
2025-01-12 22:22:24 +0530

    Time: 0 ms (100%), Space: 16.4 MB (78.74%) - LeetHub

---
+++ 1689-partitioning-into-minimum-number-of-deci-binary-numbers/1689-partitioning-into-minimum-number-of-deci-binary-numbers.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int minPartitions(string n) {
+        char max_digit = '0';
+        for (char c : n) {
+            if (c > max_digit) {
+                max_digit = c;
+            }
+        }
+        return max_digit - '0';
+        // wow!!
+    }
+};
