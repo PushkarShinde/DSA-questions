c687b076ba30ad6eac4742da883178b85911fee5
Pushkar Shinde <pushkarshinde24@gmail.com>
2025-01-18 12:50:09 +0530

    Time: 4 ms (33.61%), Space: 8.5 MB (64.41%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.cpp
+++ 0009-palindrome-number/0009-palindrome-number.cpp
@@ -2,13 +2,13 @@ class Solution {
 public:
     int reverse(int x){
         int result=0;
-        while(){
-            int rem=%10;
+        while(x){
+            int rem=x%10;
             if (result > INT_MAX/10 || result < INT_MIN/10) {
                 return 0; // Overflow condition
             }
             result=result*10+rem;
-            /=10; 
+            x/=10; 
         }
         return result;
     }
