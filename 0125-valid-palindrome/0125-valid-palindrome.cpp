fc4701975bf03626294dc90071089f121cbd8a7e
Pushkar Shinde <pushkarshinde24@gmail.com>
2025-01-27 01:25:35 +0530

    Time: 0 ms (100%), Space: 9.8 MB (94.11%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.cpp
+++ 0125-valid-palindrome/0125-valid-palindrome.cpp
@@ -1,27 +1,14 @@
 class Solution {
 public:
     bool isPalindrome(string s) {
-        int left = 0, right = s.length() - 1;
-
-        while (left < right) {
-            // Skip non-alphanumeric characters
-            while (left < right && !isalnum(s[left])) {
-                left++;
-            }
-            while (left < right && !isalnum(s[right])) {
-                right--;
-            }
-
-            // Compare characters (case-insensitive)
+        for (int left = 0, right = s.length() - 1; left < right; left++, right--) {
+            while (left < right && !isalnum(s[left])) left++;
+            while (left < right && !isalnum(s[right])) right--;
+            
             if (tolower(s[left]) != tolower(s[right])) {
                 return false;
             }
-
-            // Move pointers
-            left++;
-            right--;
         }
-
         return true;
     }
 };
