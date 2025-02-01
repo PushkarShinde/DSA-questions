b1c04369ac5fcf7102930f0cbe2a4bad44ebddc0
Pushkar Shinde <pushkarshinde24@gmail.com>
2025-02-01 11:54:52 +0530

    Time: 0 ms (100%), Space: 27.9 MB (2.79%) - LeetHub

--- 3151-special-array-i/3151-special-array-i.cpp
+++ 3151-special-array-i/3151-special-array-i.cpp
@@ -1,13 +1,13 @@
 class Solution {
 public:
     bool isArraySpecial(vector<int>& nums) {
+        if(nums.size()==1) return true;
         int n=nums.size();
-        if(n==1) return true;
-        int sum=nums[0];
-        int key=0;
+
         for(int i=1;i<n;i++){
-            sum+=nums[i];
-            if(sum%2==0) {
+            int pre=nums[i-1];
+            int post=nums[i];
+            if((pre+post)%2==0){
                 return false;
             }
         }
