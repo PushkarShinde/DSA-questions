1dbceddf2d502619ca74ea3a4d3673789c93387b
Pushkar Shinde <pushkarshinde24@gmail.com>
2025-03-05 17:04:58 +0530

    Time: 0 ms (100%), Space: 40.9 MB (9.84%) - LeetHub

--- 2579-count-total-number-of-colored-cells/2579-count-total-number-of-colored-cells.java
+++ 2579-count-total-number-of-colored-cells/2579-count-total-number-of-colored-cells.java
@@ -1,10 +1,11 @@
 class Solution {
     public long coloredCells(int n) {
-        if(n==1) return 1;
-        long result=1;
-        for(int i=2;i<=n;i++){
-            result+=4*(i-1);
-        }
-        return result;
+        // if(n==1) return 1;
+        // long result=1;
+        // for(int i=2;i<=n;i++){
+        //     result+=4*(i-1);
+        // }
+        // return result;
+        return 1+ 2L*(n-1)*n;
     }
 }
