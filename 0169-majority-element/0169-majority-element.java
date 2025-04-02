10d52195561fd41841e2d04498acd16cfddd7dbf
Pushkar Shinde <pushkarshinde24@gmail.com>
2025-04-02 12:29:19 +0530

    Time: 1 ms (99.89%), Space: 53.8 MB (8.78%) - LeetHub

--- 0169-majority-element/0169-majority-element.java
+++ 0169-majority-element/0169-majority-element.java
@@ -1,12 +1,14 @@
 class Solution {
     public int majorityElement(int[] nums) {
         /* MOST OPTIMAL
-        Boyre-Moore Voting Algorithm */
-        int ele=nums[0];
+        Boyre-Moore Voting Algorithm 
+        Agar majority pe count increase kare aur bakiyo pe decrease, toh count will always be positive!
+        */
+        int ele=nums[0]; // suppose pehla element majority hoga
         int count=0;
         int n=nums.length;
         for(int i=0;i<n;i++){
-            if(count==0) ele=nums[i];
+            if(count==0) ele=nums[i]; // agar count 0 ho gaya toh matlab existing element majority nahi tha, now change it
             if(ele==nums[i]) count++;
             else count--;
         }
