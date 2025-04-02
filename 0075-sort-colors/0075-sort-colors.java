7d5503d22b9b643d435f2d82268e4b8be2306ca7
Pushkar Shinde <pushkarshinde24@gmail.com>
2025-04-02 11:52:52 +0530

    Time: 0 ms (100%), Space: 41.9 MB (81.64%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.java
+++ 0075-sort-colors/0075-sort-colors.java
@@ -1,5 +1,18 @@
 class Solution {
+    private void swap(int[] arr, int a, int b){
+        int temp=arr[a];
+        arr[a]=arr[b];
+        arr[b]=temp;
+    }
     public void sortColors(int[] nums) {
+        /*
+        BRUTE!!
+        Arrays.sort(nums);
+        */
+
+        /*
+        BETTER!!
+
         int zero=0, one=0, two=0;
         for(int i:nums){
             if(i==0) zero++;
@@ -11,5 +24,25 @@ class Solution {
             if(i>=zero && i<one+zero) nums[i]=1;
             if(i>=one+zero && i<two+one+zero) nums[i]=2;
         }
+        */
+        
+        /*
+        OPTIMAL
+        -> DUTCH NATIONAL FLAG ALGORITHM
+        */
+        int mid=0, low=0, high=nums.length-1;
+        while(mid<=high){
+            if(nums[mid]==0){
+                swap(nums, mid, low);
+                low++; mid++;
+            } 
+            else if(nums[mid]==1) mid++;
+            else {
+                swap(nums, mid, high);
+                high--;
+            }
+        }
+
+        /* WOW!!! */
     }
 }
