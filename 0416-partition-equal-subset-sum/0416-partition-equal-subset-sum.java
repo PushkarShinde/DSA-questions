2015f1216079cca047555946503ba6fb0ab511f1
Pushkar Shinde <pushkarshinde24@gmail.com>
2025-04-07 21:06:23 +0530

    Time: 31 ms (69.71%), Space: 41.9 MB (93.11%) - LeetHub

--- 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.java
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.java
@@ -1,4 +1,13 @@
 class Solution {
+    private boolean solve(int[] nums, int sum, int i){
+        if(sum==0) return true;
+        if(i==nums.length) return false;
+        if(sum<nums[i]) return false;
+        if(sum==nums[i]) return true;
+        boolean take=solve(nums,sum-nums[i],i+1);
+        boolean notTake=solve(nums,sum,i+1);
+        return take || notTake; // wow, ab jisme bhi true aya ho humara answer true hi ayega!
+    }
     public boolean canPartition(int[] nums) {
         int n=nums.length;
         int sumFull=0;
@@ -6,11 +15,18 @@ class Solution {
             sumFull+=i;
         }
         if(sumFull%2!=0) return false;
+        ArrayList<ArrayList<Integer>>list=new ArrayList<>();
         int sumHalf=sumFull/2;
-        for(int i:nums){
-            if(i>sumHalf) return false;
-            else if(i==sumHalf) return true;
+        boolean[] dp=new boolean[sumHalf+1];
+        dp[0]=true;
+        // The dp[i] tells you: "Can we make sum = i using any subset of the array?"
+        // dp[0] = true because you can always make sum 0 — by taking nothing (the empty subset).
+        dp[0] = true; // Base case: subset sum of 0 is always possible
+        for (int num : nums) {
+            for (int i = sumHalf; i >= num; i--) {
+                dp[i] = dp[i] || dp[i - num];
+            }
         }
-        return true;
+        return dp[sumHalf];
     }
 }
