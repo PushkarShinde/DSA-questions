c74c461d3fb8fa1e4bf59d889f8b524e3cfc1323
Pushkar Shinde <pushkarshinde24@gmail.com>
2025-04-11 20:29:18 +0530

    Time: 11 ms (96.68%), Space: 41.5 MB (89.9%) - LeetHub

--- 2843-count-symmetric-integers/2843-count-symmetric-integers.java
+++ 2843-count-symmetric-integers/2843-count-symmetric-integers.java
@@ -1,5 +1,7 @@
 class Solution {
     public int countSymmetricIntegers(int low, int high) {
+        /*
+        BRUTE APPROACH.......TC- O(N^2)
         int count=0;
         for(int num=low;num<=high;num++){
             String s=String.valueOf(num);
@@ -13,6 +15,27 @@ class Solution {
             }
             if(sum1==sum2) count++;
         }
+        return count;
+        */
+
+        // OPTIMISED APPROACH...O(N)
+        // we know constraints ke hisab se max is 10^4
+        // so only even dig numbers are 10-99 and 1000-9999
+        // so...
+        int count=0;
+        for(int num=low;num<=high;num++){
+            if(num>=10 && num<=99){
+                if(num%11==0) count++;
+                // 10-99 has symmetric numbers div by 11..obviously!
+            }else if(num>=1000 && num<=9999){
+                int n1=(num/1000);
+                int n2=(num/100)%10;
+                int n3=(num/10)%10;
+                int n4=(num)%10;
+                if(n1+n2==n3+n4) count++;
+            }
+        }
+        // wow!!
         return count;
     }
 }
