ba541bb77b5d777aaaaeb8e2aed2b8b2b739581f
Pushkar Shinde <pushkarshinde24@gmail.com>
2025-04-19 17:25:46 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 0162-find-peak-element/0162-find-peak-element.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int findPeakElement(int[] nums) {
+        int n=nums.length;
+        if(n==1) return 0;
+        if(n==2) return (nums[0]>nums[1])?0:1;
+        int l=0, r=n-1;
+        // array can have multilpe peaks, koi bhi return karo usmese chalega!!
+        while(l<r){
+            int mid=l+(r-l)/2;
+            if(nums[mid]>nums[mid-1] && nums[mid]>nums[mid+1]){
+                return mid;
+            }
+            if(nums[mid]>nums[mid-1]){
+                l=mid+1;
+            }else{
+                r=mid;
+            }
+        }
+        return l;
+    }
+}
