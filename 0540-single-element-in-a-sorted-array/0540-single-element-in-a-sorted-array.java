6033ddcf1b53c55061e3214f7f136871347b7441
Pushkar Shinde <pushkarshinde24@gmail.com>
2025-04-19 16:02:07 +0530

    Time: 0 ms (100%), Space: 50.6 MB (30.45%) - LeetHub

--- 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.java
+++ 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.java
@@ -1,5 +1,12 @@
 class Solution {
     public int singleNonDuplicate(int[] nums) {
+        /*
+        The key idea is:
+            1. Every element appears twice except one.
+            2. The first instance of the pair is always at even index, 
+            and the second at odd index, before the single element.
+            3. After the single element, the pattern breaks.
+         */
         int n=nums.length;
         int l=0, r=n-1;
         if(n==1) return nums[0];
