b66d0924dcde5f5171f77c6f0c9802c2206e5bc0
Pushkar Shinde <pushkarshinde24@gmail.com>
2025-04-19 17:26:43 +0530

    Time: 0 ms (100%), Space: 42.7 MB (22.29%) - LeetHub

--- 0162-find-peak-element/0162-find-peak-element.java
+++ 0162-find-peak-element/0162-find-peak-element.java
@@ -3,6 +3,8 @@ class Solution {
         int n=nums.length;
         if(n==1) return 0;
         if(n==2) return (nums[0]>nums[1])?0:1;
+        if(nums[0]>nums[1]) return 0;
+        if(nums[n-1]>nums[n-2]) return n-1;
         int l=0, r=n-1;
         // array can have multilpe peaks, koi bhi return karo usmese chalega!!
         while(l<r){
