1e7976d5ebe372e3d3c8d1ef6b8d8e5805f4b411
Pushkar Shinde <pushkarshinde24@gmail.com>
2025-04-21 23:04:45 +0530

    Time: 5 ms (33.33%), Space: 58.7 MB (95.24%) - LeetHub

--- 2145-count-the-hidden-sequences/2145-count-the-hidden-sequences.java
+++ 2145-count-the-hidden-sequences/2145-count-the-hidden-sequences.java
@@ -10,4 +10,28 @@ class Solution {
         long maxStart = upper - maxPrefix;
         return (int)Math.max(0, maxStart - minStart + 1); // count cannot be negative
     }
+
+    //My Brute Force gives TLE beacuse it check each element in lower and upper bound
+
+   /*  public int numberOfArrays(int[] differences, int lower, int upper) {
+        int firstLastDiff = 0, n = differences.length, c =0;
+
+        for(int diff: differences) firstLastDiff+=diff;
+
+        for(int i = lower;i<=upper;i++){
+            int prev = i;
+            for(int j=0;j<n;j++){
+                prev = differences[j] + prev;
+                if(prev<lower || prev>upper){
+                    break;
+                }
+                if(j == n-1){
+                    if(prev != i+firstLastDiff) break;
+                    c++;
+                }
+            }
+        }
+
+        return c;
+    }*/
 }
