627a9cc61c4d6799b17e1e1eed9808ed50b0cb94
Pushkar Shinde <pushkarshinde24@gmail.com>
2025-04-23 20:45:32 +0530

    Time: 6 ms (65.95%), Space: 40.5 MB (77.06%) - LeetHub

--- 1399-count-largest-group/1399-count-largest-group.java
+++ 1399-count-largest-group/1399-count-largest-group.java
@@ -1,18 +1,14 @@
 class Solution {
-    private int digSum(int n){
-        int sum=0;
-        while(n>0){
-            sum+=(n%10);
-            n/=10;
-        }
-        return sum;
-    }
     public int countLargestGroup(int n) {
         // HashMap<Integer> map=new HashMap<>();
         int[] map=new int[37];
         int maxVal=0;
         for(int i=1;i<=n;i++){
-            int j=digSum(i);
+            int a=i%10;
+            int b=(i/10)%10;
+            int c=(i/100)%10;
+            int d=(i/1000)%10;
+            int j=a+b+c+d;
             map[j]++;
             maxVal=Math.max(maxVal, map[j]);
         }
