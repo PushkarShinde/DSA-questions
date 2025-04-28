656d8ba76cd04a1e56cf82372cde5f02de5e4da9
Pushkar Shinde <pushkarshinde24@gmail.com>
2025-04-28 16:14:55 +0530

    Time: 0 ms (100%), Space: 41.1 MB (53.43%) - LeetHub

--- 0100-same-tree/0100-same-tree.java
+++ 0100-same-tree/0100-same-tree.java
@@ -14,21 +14,34 @@
  * }
  */
 class Solution {
-    private void inorder(TreeNode root, List<Integer> list){
-        if(root!=null){
-            inorder(root.left,list);
-            list.add(root.val);
-            inorder(root.right,list);
-        }
-    }
+    // private void inorder(TreeNode root, List<Integer> list){
+    //     if(root!=null){
+    //         inorder(root.left,list);
+    //         list.add(root.val);
+    //         inorder(root.right,list);
+    //     }
+    // }
     public boolean isSameTree(TreeNode p, TreeNode q) {
-        List<Integer> plist=new ArrayList<>();
-        List<Integer> qlist=new ArrayList<>();
-        inorder(p, plist);
-        inorder(q, qlist);
-        if(plist.size()==qlist.size()){
-            return plist.equals(qlist);
-        }
-        return false;
+        // List<Integer> plist=new ArrayList<>();
+        // List<Integer> qlist=new ArrayList<>();
+        // inorder(p, plist);
+        // inorder(q, qlist);
+        // if(plist.size()==qlist.size()){
+        //     return plist.equals(qlist);
+        // }
+        // return false;
+
+        if(p==null && q==null) return true;
+        if(p==null || q==null) return false;
+        if(p.val!=q.val) return false;
+        return isSameTree(p.left, q.left) && isSameTree(p.right,q.right); // wow
     }
 }
+
+/**
+Understand the problem clearly:
+    Is it just value comparison?
+    Or also structure comparison?
+    If structure matters (like trees), always think recursion or DFS.
+    Avoid converting complex structures (trees) into arrays/lists unnecessarily. 
+*/
