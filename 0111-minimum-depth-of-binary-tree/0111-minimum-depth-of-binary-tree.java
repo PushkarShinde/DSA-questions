8f52e9bd6eaf95e4c94275d24906aa83a32f35dc
Pushkar Shinde <pushkarshinde24@gmail.com>
2025-05-01 12:22:42 +0530

    Time: 2 ms (87.77%), Space: 62.4 MB (98.91%) - LeetHub

--- 0111-minimum-depth-of-binary-tree/0111-minimum-depth-of-binary-tree.java
+++ 0111-minimum-depth-of-binary-tree/0111-minimum-depth-of-binary-tree.java
@@ -14,14 +14,37 @@
  * }
  */
 class Solution {
+    // // RECURSIVE DFS SOLUTION 
+    // public int minDepth(TreeNode root) {
+    //     if(root==null) return 0;
+    //     int left=minDepth(root.left);
+    //     int right=minDepth(root.right);
+    //     // agar donomese koi child null hai toh dusre ko consider karo
+    //     if(root.left==null) return right+1;
+    //     if(root.right==null) return left+1;
+    //     // agar dono exist karte hai toh dono se jo chota ho usko count karo
+    //     return Math.min(left, right)+1;
+    // }
     public int minDepth(TreeNode root) {
         if(root==null) return 0;
-        int left=minDepth(root.left);
-        int right=minDepth(root.right);
-        // agar donomese koi child null hai toh dusre ko consider karo
-        if(root.left==null) return right+1;
-        if(root.right==null) return left+1;
-        // agar dono exist karte hai toh dono se jo chota ho usko count karo
-        return Math.min(left, right)+1;
+        Queue<TreeNode> queue=new LinkedList<>();
+        queue.offer(root); 
+        int depth=1;
+        while(!queue.isEmpty()){
+            int s=queue.size();
+            for(int i=0;i<s;i++){
+                TreeNode node=queue.poll();
+                // check karo ki leaf hai kya, hoga toh abhitak ka depth return kardo, kaam ho gaya
+                if(node.left==null && node.right==null) return depth; 
+                // agar leaf nahi hoga toh aur depth me jao
+                if(node.left!=null) queue.offer(node.left);
+                if(node.right!=null) queue.offer(node.right);
+            }
+            depth++;
+        }
+        return depth;
     }
 }
+
+/**✅ Why BFS is more efficient for minDepth?
+BFS traverses level by level, and the moment it finds the first leaf node, it returns the current depth. This avoids unnecessary traversal of deeper nodes, especially useful in unbalanced trees. */
