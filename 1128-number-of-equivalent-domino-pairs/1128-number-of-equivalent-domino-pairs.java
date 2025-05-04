c862f19c878f4f8d17d439361e89a0efa71089c8
Pushkar Shinde <pushkarshinde24@gmail.com>
2025-05-04 13:08:14 +0530

    Time: 2796 ms (5.09%), Space: 52.5 MB (91.09%) - LeetHub

---
+++ 1128-number-of-equivalent-domino-pairs/1128-number-of-equivalent-domino-pairs.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int numEquivDominoPairs(int[][] dominoes) {
+        // My BRUTE APPROACH O(N^2)
+        int n=dominoes.length;
+        int count=0;
+        for(int i=0;i<n-1;i++){
+            int a=dominoes[i][0];
+            int b=dominoes[i][1];
+            for(int j=i+1;j<n;j++){
+                int c=dominoes[j][0];
+                int d=dominoes[j][1];
+                if((a == c && b == d) || (a == d && b == c)){
+                    count++;
+                }
+            }
+        }
+        return count;
+    }
+}
