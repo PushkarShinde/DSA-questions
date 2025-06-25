aa03f3025cc18e2554973cef3d2ebf5d114dc9df
Pushkar Shinde <pushkarshinde24@gmail.com>
2025-06-25 20:28:01 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0875-koko-eating-bananas/0875-koko-eating-bananas.java
+++ 0875-koko-eating-bananas/0875-koko-eating-bananas.java
@@ -1,13 +1,19 @@
 class Solution {
     public int minEatingSpeed(int[] piles, int h) {
         int n=piles.length;
-        int maxBan=Integer.MIN_VALUE;
-        for(int i: piles){
-            maxBan=Math.max(maxBan, i);
-            // minBan=Math.min(minBan, i);// not valid because kya pata h bahot bada ho
-        }
-        if(h==n) return maxBan;
-        int l=1, r=maxBan;
+        int total=0;
+        for(int i: piles) total+=i;
+        int l=(int) ((total - 1) / h) + 1;
+        // int l=Math.ceil((double)total/h);
+        //This avoids starting the binary search from 1, which is too low if the total is large.
+        int r=(int) ((total - n) / (h - n + 1)) + 1;
+        /*here’s the logic:
+        - Every pile takes at least 1 hour, no matter how fast she eats.
+        -So she needs at least n hours, one for each pile.
+        -That leaves h - n extra hours, which can help distribute some piles across more time.
+        -Based on that, the formula calculates a realistic upper bound for k — a speed beyond which 
+        increasing doesn't really help because Koko will still be limited by the per-pile requirement.
+        */
         while(l<r){
             int mid=l+(r-l)/2;
             if(time(piles,mid)>h){
