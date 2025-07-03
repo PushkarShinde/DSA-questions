da8a6b380e2d2ca95ae2e6d54693a8c3243cc506
Pushkar Shinde <pushkarshinde24@gmail.com>
2025-07-03 17:46:34 +0530

    Time: 166 ms (2.24%), Space: 42.8 MB (28.13%) - LeetHub

--- 0050-powx-n/0050-powx-n.java
+++ 0050-powx-n/0050-powx-n.java
@@ -1,7 +1,8 @@
 class Solution {
     public double myPow(double x, int n) {
         if(x==0) return 0.0;
-
+        if(n==0) return 1.0;
+        if(n==1) return x; 
         double res=1.0;
         long nn=n; // converting to long
 
@@ -19,4 +20,9 @@ class Solution {
         }
         return res;
     }
+    /**
+    If n is even → x^n = (x^2)^(n/2)
+    If n is odd → x^n = x * (x^2)^(n/2)
+    We repeat this process using the idea of exponent halving to reach O(log n) time.
+    */
 }
