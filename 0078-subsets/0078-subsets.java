028dcf692b7c16e55a083aaf80f42ff8221aafc2
Pushkar Shinde <pushkarshinde24@gmail.com>
2025-07-05 16:00:43 +0530

    Time: 0 ms (100%), Space: 43 MB (17.93%) - LeetHub

---
+++ 0078-subsets/0078-subsets.java
@@ -0,0 +1,16 @@
+class Solution {
+    public List<List<Integer>> subsets(int[] nums) {
+        int n=nums.length;
+        List<List<Integer>> ans=new ArrayList<>();
+        for(int i=0;i<(1<<n);i++){ //(1<<n) is equal to Math.pow(2,n); think about it
+            List<Integer> subset=new ArrayList<>();
+            for(int j=0;j<n;j++){
+                if((i & (1<<j)) !=0){
+                    subset.add(nums[j]);
+                }
+            }
+            ans.add(subset);
+        }
+        return ans;
+    }
+}
