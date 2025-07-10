0facba36eaed29c4baf25d0621b5c7bd3edab7fe
Pushkar Shinde <pushkarshinde24@gmail.com>
2025-07-10 17:58:38 +0530

    Time: 33 ms (39.38%), Space: 58.7 MB (63.82%) - LeetHub

--- 0239-sliding-window-maximum/0239-sliding-window-maximum.java
+++ 0239-sliding-window-maximum/0239-sliding-window-maximum.java
@@ -1,31 +1,21 @@
 class Solution {
     public int[] maxSlidingWindow(int[] nums, int k) {
         int n=nums.length;
-        int[] res=new int[n-k+1];
-        // TC: O(n), SC: O((k)
-        if(n==0) return res;
-        Deque<Integer> deque=new ArrayDeque<Integer>();
-        int ind=0;
-        while(ind<k){
-            while(!deque.isEmpty() && nums[deque.peekLast()]<=nums[ind]){
-                deque.pollLast(); // agar ane wale number se existing chota ya uske jitna hai toh hata do! 
+        int[] ans=new int[n-k+1];
+        int index=0;
+        Deque<Integer> dq=new ArrayDeque<>();
+        for(int i=0;i<n;i++){
+            while(!dq.isEmpty() && dq.peek()<i-k+1){
+                dq.pollFirst();
             }
-            deque.offerLast(ind);
-            ind++;
-        }
-        res[0]=nums[deque.peekFirst()]; // first window ke max ko resultant array me dala
-        for(int i=1;i<n-k+1;i++){ // age ane wale windows
-            while(!deque.isEmpty() && deque.peekFirst()<i){
-                deque.pollFirst(); 
-                // existing window ke starting index ko hata denge agar vo new window ke start se chota ho
-            } 
-            int newInd=i+k-1;
-            while(!deque.isEmpty() && nums[deque.peekLast()]<=nums[newInd]){
-                deque.pollLast();
+
+            while(!dq.isEmpty() && nums[i]>nums[dq.peekLast()]){
+                dq.pollLast();
             }
-            deque.offerLast(newInd); // window ko age badhayenge!
-            res[i]=nums[deque.peekFirst()];
+            dq.offer(i);
+
+            if(i>=k-1) ans[index++]=nums[dq.peek()];
         }
-        return res;
+        return ans;
     }
 }
