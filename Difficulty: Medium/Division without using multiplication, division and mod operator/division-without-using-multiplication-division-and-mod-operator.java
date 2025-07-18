f3be669301dd1e63f74221f779fd47bd1b2d568d
Pushkar Shinde <pushkarshinde24@gmail.com>
2025-07-18 12:15:19 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Division without using multiplication, division and mod operator/division-without-using-multiplication-division-and-mod-operator.java
@@ -0,0 +1,30 @@
+// User function Template for Java
+
+class Solution {
+    public static long divide(long dividend, long divisor) {
+        // WOW
+        if (dividend == divisor) return 1;
+
+        long a = Math.abs(dividend);
+        long b = Math.abs(divisor);
+        long res = 0;
+
+        while (a >= b) {
+            int power = 0;
+            while (a >= (b << (power + 1))) {
+                power++;
+            }
+            a -= (b << power);
+            res += (1L << power);
+        }
+
+        // Apply sign
+        if ((dividend < 0) ^ (divisor < 0)) res = -res;
+
+        // Clamp to 32-bit signed int range
+        if (res > Integer.MAX_VALUE) return Integer.MAX_VALUE;
+        if (res < Integer.MIN_VALUE) return Integer.MIN_VALUE;
+
+        return res;
+    }
+}
