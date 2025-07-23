98862dfe8319bd766f97e06456245b395d03e863
Pushkar Shinde <pushkarshinde24@gmail.com>
2025-07-23 15:15:34 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 0204-count-primes/0204-count-primes.java
@@ -0,0 +1,34 @@
+class Solution {
+    public int countPrimes(int n) {
+    //     int count=0; 
+    //     for(int i=2;i<n;i++){
+    //         if(isPrime(i))count++;
+    //     }
+    //     return count;
+    // }
+    // private boolean isPrime(int n) {
+    //     if(n<2) return false;
+    //     for(int i=2;i*i<=n;i++){
+    //         if(n%i==0) return false;
+    //     }
+    //     return true;
+        if(n<2) return 0;
+        boolean[] isPrime=new boolean[n+1]; 
+        Arrays.fill(isPrime, true);
+
+        isPrime[0]= isPrime[1]= false;
+
+        for(int i=2;i*i<=n;i++){
+            if(isPrime[i]==true) {
+                for(int j=i*i;j<=n;j+=i){
+                    isPrime[j]=false;
+                }
+            }
+        }
+        int count=0;
+        for(boolean i: isPrime){
+            if(i==true) count++;
+        }
+        return count;
+    }
+}
