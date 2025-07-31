e9a4533ce874e0c16c43ace259c3c6a66e6246d7
Pushkar Shinde <pushkarshinde24@gmail.com>
2025-07-31 19:53:38 +0530

    Time: 144 ms (86.07%), Space: 71.2 MB (85.25%) - LeetHub

--- 0898-bitwise-ors-of-subarrays/0898-bitwise-ors-of-subarrays.java
+++ 0898-bitwise-ors-of-subarrays/0898-bitwise-ors-of-subarrays.java
@@ -1,18 +1,18 @@
 class Solution {
-    public int subarrayBitwiseORs(int[] arr) {
-        Set<Integer> res=new HashSet<>();
-        Set<Integer> pre=new HashSet<>();
-        for(int i: arr){
-            Set<Integer> cur = new HashSet<>();
-            cur.add(i);
-            for(int x: pre){
-                cur.add(x|i);
-            }
-            pre=cur;
-            res.addAll(cur);
-        }
-        return res.size();
-    }
+    // public int subarrayBitwiseORs(int[] arr) {
+    //     Set<Integer> res=new HashSet<>();
+    //     Set<Integer> pre=new HashSet<>();
+    //     for(int i: arr){
+    //         Set<Integer> cur = new HashSet<>();
+    //         cur.add(i);
+    //         for(int x: pre){
+    //             cur.add(x|i);
+    //         }
+    //         pre=cur;
+    //         res.addAll(cur);
+    //     }
+    //     return res.size();
+    // }
     /**
     At every index, you keep track of all possible OR values ending at that index using a set. Here's the plan:
     Let cur be the set of ORs that can be formed ending at arr[i].
@@ -22,4 +22,17 @@ class Solution {
     All (x | arr[i]) for x ∈ prev
     You add everything from cur into a global Set so duplicates are removed automatically.
      */
+    public int subarrayBitwiseORs(int[] arr) {
+        int n=arr.length;
+        Set<Integer> res=new HashSet<>();
+        for(int i=0;i<n;i++){
+            res.add(arr[i]);
+            for(int j=i-1;j>=0;j--){
+                if(arr[j]==(arr[i]|arr[j])) break;
+                arr[j]|=arr[i];
+                res.add(arr[j]);
+            }
+        }
+        return res.size();
+    }
 }
