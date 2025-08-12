2a0f2c1c41dd3f7721dd0eebf7653eb654245fcf
Pushkar Shinde <pushkarshinde24@gmail.com>
2025-08-12 11:49:21 +0530

    Time: 82 ms (65.26%), Space: 41.4 MB (100%) - LeetHub

--- 2787-ways-to-express-an-integer-as-sum-of-powers/2787-ways-to-express-an-integer-as-sum-of-powers.java
+++ 2787-ways-to-express-an-integer-as-sum-of-powers/2787-ways-to-express-an-integer-as-sum-of-powers.java
@@ -1,5 +1,10 @@
 class Solution {
+    private static final int mod=(int)1e9+7;
+    private static int[][] memo=new int[301][301];
     public int numberOfWays(int n, int x) {
+        for (int i = 0; i <= n; i++) {
+            Arrays.fill(memo[i], -1);
+        }
         // List<Integer> powers=new ArrayList<>();
         // int num=1;
         // while(true){
@@ -14,14 +19,22 @@ class Solution {
         return solve(n, x, 1);
     }
     private int solve(int n, int x, int num){
+        // Arrays.fill(memo, -1);
         if(n==0) return 1;
         if(n<0) return 0;
-        int power=(int)Math.pow(num, x);
+        int power=intPow(num, x);
         if(power>n) return 0;
-
+        if(memo[n][num]!=-1) return memo[n][num];
         int take=solve(n-power, x, num+1);
         int notTake=solve(n, x, num+1);
 
-        return take+notTake;
+        return memo[n][num]=(take+notTake)%mod;
+    }
+    private int intPow(int base, int exp) {
+        int result = 1;
+        for (int i = 0; i < exp; i++) {
+            result *= base;
+        }
+        return result;
     }
 }
