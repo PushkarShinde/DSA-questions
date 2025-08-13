bc607d9725b39f12c7cf41dd2d829b924711df4d
Pushkar Shinde <pushkarshinde24@gmail.com>
2025-08-13 10:11:29 +0530

    Time: 8 ms (84.41%), Space: 43.7 MB (97.57%) - LeetHub

--- 0326-power-of-three/0326-power-of-three.java
+++ 0326-power-of-three/0326-power-of-three.java
@@ -19,8 +19,12 @@ class Solution {
     //     return false;
     // }
 
-        if(n<1) return false;
-        while(m%3==0) m/=3;
-        return m==1;
+        // if(n<1) return false;
+        // while(m%3==0) m/=3;
+        // return m==1;
+
+        if (n <= 0) return false;
+        double logVal = Math.log10(n) / Math.log10(3);
+        return Math.abs(logVal - Math.round(logVal)) < 1e-10; // integer check
     }
 }
