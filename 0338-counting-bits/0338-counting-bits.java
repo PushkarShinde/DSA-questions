a106b98b5b9bdd2f8f43d37ba116c460f3275561
Pushkar Shinde <pushkarshinde24@gmail.com>
2025-09-22 21:57:37 +0530

    Time: 2 ms (94.05%), Space: 46.8 MB (51.93%) - LeetHub

--- 0338-counting-bits/0338-counting-bits.java
+++ 0338-counting-bits/0338-counting-bits.java
@@ -1,18 +1,9 @@
 class Solution {
     public int[] countBits(int n) {
-        int[] num=new int[n+1];
-        for(int i=0;i<n+1;i++){
-            num[i]=solve(i);
+        int[] res=new int[n+1];
+        for(int i=0;i<=n;i++){
+            res[i]=res[i>>1]+(i&1);
         }
-        return num;
-    }
-    private int solve(int n){
-        int count=0;
-        int i=1;
-        while(i<=n){
-            if((n&i)!=0) count++;
-            i<<=1;
-        }
-        return count;
+        return res;
     }
 }
