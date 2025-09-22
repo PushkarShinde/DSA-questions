4f1b869f9619e40490d43d95534e0fa11799eaf1
Pushkar Shinde <pushkarshinde24@gmail.com>
2025-09-22 20:29:51 +0530

    Time: 0 ms (100%), Space: 40.8 MB (18.76%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.java
+++ 0509-fibonacci-number/0509-fibonacci-number.java
@@ -6,14 +6,30 @@ class Solution {
     // }
     
     //memoization
+    // public int fib(int n) {
+    //     int[] memo=new int[n+1];
+    //     Arrays.fill(memo,-1);
+    //     return solve(n,memo);
+    // }
+    // private int solve(int n,int[] memo){
+    //     if(n<=1) {
+    //         memo[n]=n;
+    //         return memo[n];
+    //     }
+    //     if(memo[n]!=-1) return memo[n];
+    //     memo[n]=fib(n-1)+fib(n-2);
+    //     return memo[n];
+    // }
+
+    //BOTTUMN UP
     public int fib(int n) {
-        int[] memo=new int[n+1];
-        return solve(n,memo);
-    }
-    private int solve(int n,int[] memo){
+        int[] dp=new int[n+1];
         if(n<=1) return n;
-        if(memo[n]!=0) return memo[n];
-        memo[n]=fib(n-1)+fib(n-2);
-        return memo[n];
+        dp[0]=0;
+        dp[1]=1;
+        for(int i=2;i<=n;i++){
+            dp[i]=dp[i-1]+dp[i-2];
+        }
+        return dp[n]; 
     }
 }
