dbda15ecd7ede2f718f494e24f2075301ae5ce82
Pushkar Shinde <pushkarshinde24@gmail.com>
2025-10-02 20:31:06 +0530

    Time: 0 ms (100%), Space: 41.7 MB (70.32%) - LeetHub

--- 0101-symmetric-tree/0101-symmetric-tree.java
+++ 0101-symmetric-tree/0101-symmetric-tree.java
@@ -14,18 +14,15 @@
  * }
  */
 class Solution {
-    private boolean isMirror(TreeNode left, TreeNode right){
+    public boolean isSymmetric(TreeNode root) {
+        return mirror(root.left, root.right);
+    }
+    private boolean mirror(TreeNode left, TreeNode right){
         if(left==null && right==null) return true;
         if(left==null || right==null) return false;
-        // Check if:
-        // 1. Current nodes have the same value
-        // 2. Left's left matches right's right
-        // 3. Left's right matches right's left
-        return left.val==right.val && isMirror(left.left,right.right) && isMirror(left.right,right.left);
+        
+        return left.val==right.val && 
+        mirror(left.left, right.right) && 
+        mirror(left.right, right.left);
     }
-    public boolean isSymmetric(TreeNode root) {
-        if(root==null) return true;
-        return isMirror(root.left, root.right);
-    }
-    // wow
 }
