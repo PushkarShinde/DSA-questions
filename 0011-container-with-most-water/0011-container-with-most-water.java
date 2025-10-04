1318352c098d560526f66601cc1951889bd9d01f
Pushkar Shinde <pushkarshinde24@gmail.com>
2025-10-04 15:26:02 +0530

    Time: 5 ms (77.27%), Space: 57.1 MB (91.64%) - LeetHub

---
+++ 0011-container-with-most-water/0011-container-with-most-water.java
@@ -0,0 +1,30 @@
+class Solution {
+    //TC- O(n^2)
+    // public int maxArea(int[] height) {
+    //     int n=height.length;
+    //     int max=0;
+    //     for(int i=0;i<n-1;i++){
+    //         for(int j=n-1;j>i;j--){
+    //             max=Math.max(Math.min(height[i],height[j])*(j-i),max);
+    //         }
+    //     }
+    //     return max;
+    // }
+    public int maxArea(int[] height) {
+        int n=height.length;
+        int l=0, r=n-1;
+        int max=0;
+        while(l<r){
+            int hl=height[l];
+            int hr=height[r];
+            int w=Math.min(hl,hr)*(r-l);
+            max=Math.max(max,w);
+            if(hl<hr){
+                l++;
+            }else{
+                r--;
+            }
+        }
+        return max;
+    }
+}
