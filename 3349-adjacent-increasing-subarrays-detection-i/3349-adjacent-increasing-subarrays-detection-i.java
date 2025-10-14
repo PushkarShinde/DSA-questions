fc0fe9b6e47bee14238f3c954d0b2d7d791ab5c0
Pushkar Shinde <pushkarshinde24@gmail.com>
2025-10-14 16:34:20 +0530

    Time: 2 ms (47.79%), Space: 44.3 MB (91.18%) - LeetHub

--- 3349-adjacent-increasing-subarrays-detection-i/3349-adjacent-increasing-subarrays-detection-i.java
+++ 3349-adjacent-increasing-subarrays-detection-i/3349-adjacent-increasing-subarrays-detection-i.java
@@ -1,20 +1,17 @@
 class Solution {
     public boolean hasIncreasingSubarrays(List<Integer> nums, int k) {
         int n=nums.size();
-        if(k==1) return true;
-        boolean[] inc=new boolean[n-k+1];
-        Arrays.fill(inc, true);
-        for(int i=0;i<=n-k;i++){
-            for(int j=i;j<i+k-1;j++){
-                if(nums.get(j)>=nums.get(j+1)){
-                    inc[i]=false;
-                    break;
-                }
+        for(int i=0;i+2*k<=n;i++){
+            if(inc(nums, i, i+k-1) && inc(nums, i+k, i+2*k-1)){
+                return true;
             }
         }
-        for(int i=0;i+k<inc.length;i++){
-            if(inc[i]==true && inc[i+k]==true) return true;
-        }
         return false;
+    }
+    private boolean inc(List<Integer> nums, int i, int j){
+        for(int k=i;k<j;k++){
+            if(nums.get(k)>=nums.get(k+1)) return false;
+        }
+        return true;
     }
 }
