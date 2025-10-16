758a530b2f560e137c0d94c5cfb8e983254f12fc
Pushkar Shinde <pushkarshinde24@gmail.com>
2025-10-16 12:15:06 +0530

    Time: 67 ms (25.38%), Space: 65.6 MB (5.58%) - LeetHub

--- 2598-smallest-missing-non-negative-integer-after-operations/2598-smallest-missing-non-negative-integer-after-operations.java
+++ 2598-smallest-missing-non-negative-integer-after-operations/2598-smallest-missing-non-negative-integer-after-operations.java
@@ -1,16 +1,16 @@
 class Solution {
     public int findSmallestInteger(int[] nums, int value) {
         int n=nums.length;
-        Set<Integer> set=new TreeSet<>();
-        for(int i=0;i<n;i++){
-            if(nums[i]<0){
-                set.add(value-(Math.abs(nums[i])%value));
-            }
-            set.add(nums[i]%value);
+        Map<Integer, Integer> map=new HashMap<>();
+        for(int x: nums){
+            int mod = ((x % value) + value) % value;
+            map.put(mod,map.getOrDefault(mod,0)+1);
         }
         int mex=0;
         while(true){
-            if(!set.contains(mex)) break;
+            int rem=mex%value;
+            if(!map.containsKey(rem) || map.get(rem)==0) break;
+            map.put(rem,map.get(rem)-1);
             mex++;
         }
         return mex;
