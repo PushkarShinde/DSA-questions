e100d0457766a0e486433f1bfeac9092113f577c
Pushkar Shinde <pushkarshinde24@gmail.com>
2025-10-23 20:14:51 +0530

    Time: 3 ms (85.69%), Space: 45 MB (42.12%) - LeetHub

--- 3461-check-if-digits-are-equal-in-string-after-operations-i/3461-check-if-digits-are-equal-in-string-after-operations-i.java
+++ 3461-check-if-digits-are-equal-in-string-after-operations-i/3461-check-if-digits-are-equal-in-string-after-operations-i.java
@@ -1,26 +1,27 @@
 class Solution {
     public boolean hasSameDigits(String s) {
         char[] ch=s.toCharArray();
-        while(!same(ch) && ch.length>1){
+        while(ch.length>2){
             ch=solve(ch);
         }
-        return ch.length>1;
+        return same(ch);
     }
     private char[] solve(char[] ch){
         int n=ch.length;
         int[] num=new int[n];
         for(int i=0;i<n;i++){
-            num[i]=ch[i]-'0';
+            num[i]=(int)(ch[i]-'0');
         }
         char[] res=new char[n-1];
         for(int i=0;i<n-1;i++){
-            res[i]=(char)((num[i]+num[i+1])%10);
+            res[i]=(char)(((num[i]+num[i+1])%10)+'0');
         }
         return res;
     }
     private boolean same(char[] ch){
-        for(int i=1;i<ch.length;i++){
-            if(ch[i]!=ch[i-1]){
+        if(ch.length==1) return true;
+        for(int i=0;i<ch.length-1;i++){
+            if(ch[i]!=ch[i+1]){
                 return false;
             }
         }
