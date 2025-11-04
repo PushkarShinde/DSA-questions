cb49040d513624e4a00322ef2688aa22ca97f51b
Pushkar Shinde <pushkarshinde24@gmail.com>
2025-11-04 17:37:45 +0530

    Time: 0 ms (100%), Space: 43.5 MB (5.42%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
@@ -1,28 +1,24 @@
 class Solution {
-    private int BS(int[] arr, int t){
-        int n=arr.length;
-        int l=0,r=n-1;
-        int mid=0;
-        while(l<=r){
-            mid=l+(r-l)/2;
-            if(arr[mid]==t) return mid;
-            if(arr[mid]>=arr[l]){ // means the left half is sorted
-                if(arr[mid]>=t && arr[l]<=t){
-                    r=mid-1;
+    public int search(int[] nums, int target) {
+        int n=nums.length;
+        int left=0, right=n-1;
+        while(left<=right){
+            int mid=left+(right-left)/2;
+            if(nums[mid]==target) return mid;
+            if(nums[left]<=nums[mid]){//left is sorted
+                if(target>=nums[left] && target<nums[mid]){
+                    right=mid-1;
                 }else{
-                    l=mid+1;
+                    left=mid+1;
                 }
-            }else{ // means right half is sorted 
-                if(arr[mid]<=t && arr[r]>=t){
-                    l=mid+1;
+            }else if(nums[right]>=nums[mid]){//left is sorted
+                if(target<=nums[right] && target>nums[mid]){
+                    left=mid+1;
                 }else{
-                    r=mid-1;
+                    right=mid-1;
                 }
             }
-        }
+        } 
         return -1;
-    }
-    public int search(int[] nums, int target) {
-        return BS(nums, target);
     }
 }
