5f5df2463171829e006fe7135c2538be887dd92b
Pushkar Shinde <pushkarshinde24@gmail.com>
2025-11-09 20:00:27 +0530

    Time: 1 ms (90.35%), Space: 42.3 MB (11.81%) - LeetHub

---
+++ 2169-count-operations-to-obtain-zero/2169-count-operations-to-obtain-zero.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int countOperations(int num1, int num2) {
+        int count=0;
+        while(num1>0 && num2>0){
+            if(num1<num2)num2=Math.abs(num1-num2);
+            else num1=Math.abs(num1-num2);
+            count++;
+        }
+        return count;
+    }
+}
