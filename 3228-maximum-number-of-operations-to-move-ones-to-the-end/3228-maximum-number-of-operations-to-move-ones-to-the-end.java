3a77490f74a3df43df38b07d5981c5ab902a2e5f
Pushkar Shinde <pushkarshinde24@gmail.com>
2025-11-13 20:14:24 +0530

    Time: 7 ms (78.52%), Space: 47.2 MB (25.93%) - LeetHub

--- 3228-maximum-number-of-operations-to-move-ones-to-the-end/3228-maximum-number-of-operations-to-move-ones-to-the-end.java
+++ 3228-maximum-number-of-operations-to-move-ones-to-the-end/3228-maximum-number-of-operations-to-move-ones-to-the-end.java
@@ -1,22 +1,33 @@
 class Solution {
     public int maxOperations(String s) {
-        List<Integer> one=new ArrayList<>();
+        // List<Integer> one=new ArrayList<>();
+        // int n=s.length();
+        // for(int i=0;i<n;i++){
+        //     if(s.charAt(i)=='1') one.add(i);
+        // }
+        // int cost=0;
+        // for(int i=0;i<one.size()-1;i++){
+        //     int pre=one.get(i);
+        //     int post=one.get(i+1);
+        //     if(pre+1!=post){
+        //         cost+=i+1;
+        //     }
+        // }
+        // int m=one.size();
+        // if(m!=0 && one.get(m-1)!=n-1){
+        //     cost+=one.size();
+        // }
+        // return cost; 
         int n=s.length();
+        int countOne=0;
+        int op=0;
         for(int i=0;i<n;i++){
-            if(s.charAt(i)=='1') one.add(i);
-        }
-        int cost=0;
-        for(int i=0;i<one.size()-1;i++){
-            int pre=one.get(i);
-            int post=one.get(i+1);
-            if(pre+1!=post){
-                cost+=i+1;
+            if(s.charAt(i)=='1'){
+                countOne++;
+            }else if(i+1<n && s.charAt(i+1)=='1' || i==n-1){
+                op+=countOne;
             }
         }
-        int m=one.size();
-        if(m!=0 && one.get(m-1)!=n-1){
-            cost+=one.size();
-        }
-        return cost; 
+        return op;
     }
 }
