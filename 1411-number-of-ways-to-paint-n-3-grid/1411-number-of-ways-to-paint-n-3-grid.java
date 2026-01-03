95e469fd96c409b6a8f257412140fbcff06eb74d
Pushkar Shinde <pushkarshinde24@gmail.com>
2026-01-03 19:12:05 +0530

    Time: 3 ms (93.85%), Space: 42 MB (79.49%) - LeetHub

--- 1411-number-of-ways-to-paint-n-3-grid/1411-number-of-ways-to-paint-n-3-grid.java
+++ 1411-number-of-ways-to-paint-n-3-grid/1411-number-of-ways-to-paint-n-3-grid.java
@@ -1,17 +1,7 @@
 class Solution {
-    int[][][][] dp;
     private int mod=(int)1e9+7;
-    public int numOfWays(int n) {
-        dp=new int[n][4][4][4];
-        for(int i=0;i<n;i++){
-            for(int j=0;j<4;j++){
-                for(int k=0;k<4;k++){
-                    Arrays.fill(dp[i][j][k],-1);
-                }
-            }
-        }
-        return solve(0,n,-1,-1,-1);
-    }
+    /*
+    int[][][][] dp;
     private int solve(int i, int n, int pre1, int pre2, int pre3){
         if(i==n){
             return 1;
@@ -28,4 +18,36 @@ class Solution {
         }
         return dp[i][pre1+1][pre2+1][pre3+1]=total;
     }
+    */
+    public int numOfWays(int n) {
+        /*
+        dp=new int[n][4][4][4];
+        for(int i=0;i<n;i++){
+            for(int j=0;j<4;j++){
+                for(int k=0;k<4;k++){
+                    Arrays.fill(dp[i][j][k],-1);
+                }
+            }
+        }
+        return solve(0,n,-1,-1,-1);
+        */
+        long a=6;//ABA
+        long b=6;//ABC
+        for(int i=1;i<n;i++){
+            long na=(3*a+2*b)%mod;
+            long nb=(2*a+2*b)%mod;
+            a=na;
+            b=nb;
+        }
+        return (int)((a+b)%mod);
+    }
 }
+/*
+For an ABA row:
+    It can become 3 ABA
+    Or 2 ABC
+
+For an ABC row:
+    It can become 2 ABA
+    Or 2 ABC
+ */
