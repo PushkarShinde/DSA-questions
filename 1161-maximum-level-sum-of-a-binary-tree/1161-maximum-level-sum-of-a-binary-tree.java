d45e70e9452ee05b5f6be95e24672b8e84875864
Pushkar Shinde <pushkarshinde24@gmail.com>
2026-01-06 11:30:08 +0530

    Time: 9 ms (64.42%), Space: 49.3 MB (26.34%) - LeetHub

--- 1161-maximum-level-sum-of-a-binary-tree/1161-maximum-level-sum-of-a-binary-tree.java
+++ 1161-maximum-level-sum-of-a-binary-tree/1161-maximum-level-sum-of-a-binary-tree.java
@@ -13,34 +13,36 @@
  *     }
  * }
  */
-class Node{
-    TreeNode node;
-    int level;
-    Node(TreeNode node, int level){
-        this.node=node;
-        this.level=level;
-    }
-}
+// class Node{
+//     TreeNode node;
+//     int level;
+//     Node(TreeNode node, int level){
+//         this.node=node;
+//         this.level=level;
+//     }
+// }
 class Solution {
     public int maxLevelSum(TreeNode root) {
-        Queue<Node> q=new LinkedList<>();
-        q.offer(new Node(root,1));
-        Map<Integer, Integer> map=new HashMap<>();
+        Queue<TreeNode> q=new LinkedList<>();
+        q.offer(root);
+        // Map<Integer, Integer> map=new HashMap<>();
+        int level=1;
+        int res=1;
+        int maxSum=Integer.MIN_VALUE;
         while(!q.isEmpty()){
-            Node cur=q.poll();
-            TreeNode node=cur.node; 
-            int level=cur.level;
-            map.put(level,map.getOrDefault(level,0)+node.val);
-            if(node.left!=null)q.offer(new Node(node.left,level+1));
-            if(node.right!=null)q.offer(new Node(node.right,level+1));
-        }
-        int minSum=Integer.MIN_VALUE;
-        int res=Integer.MAX_VALUE;
-        for(int key:map.keySet()){
-            if(minSum<map.get(key)){
-                res=key;
-                minSum=map.get(key);
+            int size=q.size();
+            int sum=0;
+            for(int i=0;i<size;i++){
+                TreeNode cur=q.poll();
+                sum+=cur.val;
+                if(cur.left!=null)q.offer(cur.left);
+                if(cur.right!=null)q.offer(cur.right);
             }
+            if(maxSum<sum){
+                res=level;
+                maxSum=sum;
+            }
+            level++;
         }
         return res;
     }
