eabb103c05f3d5f2b1d2d4be94c4abbfd749a97d
Pushkar Shinde <pushkarshinde24@gmail.com>
2026-02-05 10:00:52 +0530

    Time: 1 ms (98.72%), Space: 46.8 MB (30.77%) - LeetHub

---
+++ 3379-transformed-array/3379-transformed-array.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int[] constructTransformedArray(int[] nums) {
+        int n=nums.length;
+        int[] res=new int[n];
+        for(int i=0;i<n;i++){
+            if(nums[i]==0) continue;
+            res[i]=nums[(((i+nums[i])%n)+n)%n];
+        }
+        return res;
+    }
+}
