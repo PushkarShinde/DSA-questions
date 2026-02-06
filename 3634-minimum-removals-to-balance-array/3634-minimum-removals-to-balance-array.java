d0caf54b2d200b7ea62a0a32f68af6657d61d491
Pushkar Shinde <pushkarshinde24@gmail.com>
2026-02-06 11:01:04 +0530

    Time: 26 ms (21.29%), Space: 86.1 MB (60.24%) - LeetHub

--- 3634-minimum-removals-to-balance-array/3634-minimum-removals-to-balance-array.java
+++ 3634-minimum-removals-to-balance-array/3634-minimum-removals-to-balance-array.java
@@ -2,18 +2,13 @@ class Solution {
     public int minRemoval(int[] nums, int k) {
         int n=nums.length;
         Arrays.sort(nums);
-        TreeMap<Integer, List<Integer>> map=new TreeMap<>();
-        for(int i=0;i<n;i++){
-            map.computeIfAbsent(nums[i], x->new ArrayList<>()).add(i);
-        }
-        int keep=0;
-        for(int i=0;i<n;i++){
-            int cur=nums[i];
-            Integer key=map.floorKey(cur*k);
-            if(key==null) continue;
-            List<Integer> pos=map.get(key);
-            int r=pos.get(pos.size()-1);
-            keep=Math.max(keep, r-i+1);
+        int keep=0, r=0;
+        for(int l=0;l<n;l++){
+            int cur=nums[l];
+            while(r<n && nums[r]<=(long)cur*k){
+                keep=Math.max(keep, r-l+1);
+                r++;
+            }
         }
         return n-keep;
     }
