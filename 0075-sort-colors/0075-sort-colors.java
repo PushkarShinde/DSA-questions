6dfd77cf84f9859448e01eb255f28ebd3eec83b7
Pushkar Shinde <pushkarshinde24@gmail.com>
2026-02-08 11:03:18 +0530

    Time: 0 ms (100%), Space: 43.5 MB (58.25%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.java
+++ 0075-sort-colors/0075-sort-colors.java
@@ -1,48 +1,24 @@
 class Solution {
-    private void swap(int[] arr, int a, int b){
-        int temp=arr[a];
-        arr[a]=arr[b];
-        arr[b]=temp;
-    }
     public void sortColors(int[] nums) {
-        /*
-        BRUTE!!
-        Arrays.sort(nums);
-        */
-
-        /*
-        BETTER!!
-
-        int zero=0, one=0, two=0;
-        for(int i:nums){
-            if(i==0) zero++;
-            if(i==1) one++;
-            if(i==2) two++;
-        }
-        for(int i=0;i<nums.length;i++){
-            if(i<zero) nums[i]=0;
-            if(i>=zero && i<one+zero) nums[i]=1;
-            if(i>=one+zero && i<two+one+zero) nums[i]=2;
-        }
-        */
-        
-        /*
-        OPTIMAL
-        -> DUTCH NATIONAL FLAG ALGORITHM
-        */
-        int mid=0, low=0, high=nums.length-1;
-        while(mid<=high){
-            if(nums[mid]==0){
-                swap(nums, mid, low);
-                low++; mid++;
-            } 
-            else if(nums[mid]==1) mid++;
-            else {
-                swap(nums, mid, high);
-                high--;
+        int n=nums.length;
+        int l=0, r=n-1;
+        int m=0;
+        while(m<=r){
+            if(nums[m]==0){
+                swap(nums, l, m);
+                l++;
+                m++;
+            }else if(nums[m]==1){
+                m++;
+            }else if(nums[m]==2){
+                swap(nums, m, r);
+                r--;
             }
         }
-
-        /* WOW!!! */
+    }
+    private void swap(int[] nums, int i, int j){
+        int temp=nums[i];
+        nums[i]=nums[j];
+        nums[j]=temp;
     }
 }
