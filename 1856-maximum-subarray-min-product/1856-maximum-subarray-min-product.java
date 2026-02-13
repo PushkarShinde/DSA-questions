84effc474c60057e689ba5d7cd0356fcc0fb6a01
Pushkar Shinde <pushkarshinde24@gmail.com>
2026-02-14 00:26:37 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 1856-maximum-subarray-min-product/1856-maximum-subarray-min-product.java
@@ -0,0 +1,34 @@
+class Solution {
+    private int mod=(int)1e9+7;
+    public int maxSumMinProduct(int[] nums){
+        int n=nums.length;
+        int[] pre=new int[n+1];
+        for(int i=0;i<n;i++){
+            pre[i+1]=pre[i]+nums[i];
+        }
+        Deque<Integer> stack=new ArrayDeque<>();
+        int[] left=new int[n];
+        for(int i=0;i<n;i++){
+            while(!stack.isEmpty() && nums[stack.peek()]>=nums[i]){
+                stack.pop();
+            }
+            left[i]=(stack.isEmpty())?-1:stack.peek();
+            stack.push(i);
+        }
+        stack.clear();
+        int[] right=new int[n];
+        for(int i=n-1;i>=0;i--){
+            while(!stack.isEmpty() && nums[stack.peek()]>=nums[i]){
+                stack.pop();
+            }
+            right[i]=(stack.isEmpty())?n:stack.peek();
+            stack.push(i);
+        }
+        long ans=0;
+        for(int i=0;i<n;i++){
+            long sum=pre[right[i]]-pre[left[i]+1];
+            ans=Math.max(ans,nums[i]*sum);
+        }
+        return (int)ans%mod;
+    }
+}
