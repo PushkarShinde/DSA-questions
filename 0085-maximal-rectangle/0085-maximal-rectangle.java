6d724fe4cf43addef35cdd0a601cdee97931e470
Pushkar Shinde <pushkarshinde24@gmail.com>
2026-02-16 01:30:37 +0530

    Time: 19 ms (39.82%), Space: 51.9 MB (6.81%) - LeetHub

--- 0085-maximal-rectangle/0085-maximal-rectangle.java
+++ 0085-maximal-rectangle/0085-maximal-rectangle.java
@@ -1,38 +1,25 @@
 class Solution {
     public int maximalRectangle(char[][] matrix) {
-        int m=matrix[0].length;
-        int n=matrix.length;
-        int[] ht=new int[m];
-        int maxArea=0;
-        for(int i=0;i<n;i++){
-            for(int j=0;j<m;j++){
-                if(matrix[i][j]=='1') ht[j]++;
-                else ht[j]=0;
-            }
-            int[] nse=new int[m];
-            int[] pse=new int[m];
-            Arrays.fill(nse,m);
-            Arrays.fill(pse,-1);
-            Stack<Integer> st=new Stack<>();
-            for(int k=m-1;k>=0;k--){
-                while(!st.isEmpty() && ht[st.peek()]>=ht[k]){
-                    st.pop();
+        int row=matrix.length;
+        int c=matrix[0].length;
+        int[][] mat=new int[row][c];
+        for(int j=0;j<c;j++){
+            for(int i=0;i<row;i++){
+                if(matrix[i][j]=='0') mat[i][j]=0;
+                else{
+                    mat[i][j]=(i>0)?mat[i-1][j]+1:1;
                 }
-                nse[k]=st.isEmpty()?m:st.peek();
-                st.push(k);
-            }
-            st.clear();
-            for(int k=0;k<m;k++){
-                while(!st.isEmpty() && ht[st.peek()]>=ht[k]){
-                    st.pop();
-                }
-                pse[k]=st.isEmpty()?-1:st.peek();
-                st.push(k);
-            }
-            for(int j=0;j<m;j++){
-                maxArea=Math.max(maxArea,(nse[j]-pse[j]-1)*ht[j]);
             }
         }
-        return maxArea;
+        int res=0;
+        for(int i=0;i<row;i++){
+            for(int j=0;j<c;j++){
+                int l=j, r=j;
+                while(l>=0 && mat[i][l]>=mat[i][j]) l--;
+                while(r<c && mat[i][r]>=mat[i][j]) r++;
+                res=Math.max(res, (r-l-1)*mat[i][j]);
+            }
+        }
+        return res;
     }
 }
