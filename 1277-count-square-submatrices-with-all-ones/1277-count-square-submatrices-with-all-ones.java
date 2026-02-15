dcc730b1a0d9a52b25b9d556518adf26833960cb
Pushkar Shinde <pushkarshinde24@gmail.com>
2026-02-16 02:16:55 +0530

    Time: 10 ms (11.17%), Space: 61.8 MB (47.58%) - LeetHub

--- 1277-count-square-submatrices-with-all-ones/1277-count-square-submatrices-with-all-ones.java
+++ 1277-count-square-submatrices-with-all-ones/1277-count-square-submatrices-with-all-ones.java
@@ -1,43 +1,30 @@
 class Solution {
-
-    // private int solve(int i, int j, int[][] matrix, int[][] memo){
-    //     int m=matrix.length;
-    //     int n=matrix[0].length;
-    //     int count=0;
-    //     if(i>=m || j>=n) return 0;
-    //     if(memo[i][j]!=-1) return memo[i][j];
-    //     if(matrix[i][j]==0) return 0;
-    //     int right=solve(i, j+1, matrix,memo);
-    //     int diag=solve(i+1, j+1, matrix,memo);
-    //     int down=solve(i+1, j, matrix,memo);
-    //     memo[i][j] = 1 + Math.min(right, Math.min(diag, down));
-    //     return memo[i][j];
-    // }
-
-    //BOTTUMN UP APPROACH TC- O(N*M)
+    int[][] dp;
     public int countSquares(int[][] matrix) {
-        int count=0;
         int m=matrix.length;
         int n=matrix[0].length;
-        int[][] dp = new int[m][n];
-        // int[][] memo = new int[m][n];
-        // for (int i = 0; i < m; i++) {
-        //     Arrays.fill(memo[i], -1);
-        // }
+        int res=0;
+        dp=new int[m][n];
+        for(int[] d:dp){
+            Arrays.fill(d, -1);
+        }
         for(int i=0;i<m;i++){
             for(int j=0;j<n;j++){
                 if(matrix[i][j]==1){
-                    // count+=solve(i,j,matrix,memo);
-                    if(i==0 || j==0){
-                        dp[i][j]=1;
-                    }else{
-                        dp[i][j]=1+Math.min(dp[i-1][j],
-                                    Math.min(dp[i][j-1],dp[i-1][j-1]));
-                    }
-                    count+=dp[i][j];
+                    res+=solve(matrix, i,j);
                 }
             }
         }
-        return count;
+        return res;
+    }
+    private int solve(int[][] grid, int i, int j){
+        int m=grid.length;
+        int n=grid[0].length;
+        if(i>=m || j>=n || grid[i][j]==0) return 0;
+        if(dp[i][j]!=-1) return dp[i][j];
+        int right=solve(grid, i+1,j);
+        int diag=solve(grid, i+1,j+1);
+        int left=solve(grid, i,j+1);
+        return dp[i][j]=1+Math.min(right, Math.min(left, diag));
     }
 }
