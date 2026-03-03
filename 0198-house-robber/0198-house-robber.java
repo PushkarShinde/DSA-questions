7664c4ab0ad33e78e217b61f93610b8bb116b306
Pushkar Shinde <pushkarshinde24@gmail.com>
2026-03-03 23:21:00 +0530

    Time: 0 ms (100%), Space: 43.1 MB (17.05%) - LeetHub

--- 0198-house-robber/0198-house-robber.java
+++ 0198-house-robber/0198-house-robber.java
@@ -1,26 +1,18 @@
 class Solution {
+    int[][] dp;
     public int rob(int[] nums) {
         int n=nums.length;
-        // int[] dp=new int[n];
-        // Arrays.fill(dp,-1);
-        return solve(nums, 0, n-1);
-    }
-    // private int solve(int[] nums, int[] dp, int i){
-    //     if(i<0) return 0;
-    //     if(i==0) return nums[i];
-    //     if(dp[i]!=-1) return dp[i];
-    //     int pick=nums[i]+solve(nums, dp, i-2);
-    //     int notpick=solve(nums, dp, i-1);
-    //     dp[i]=Math.max(pick, notpick);
-    //     return dp[i];
-    // }
-    private int solve(int[] nums, int start, int end){
-        int pre=0, cur=0;
-        for(int i=start;i<=end;i++){
-            int temp=cur;
-            cur=Math.max(cur, nums[i]+pre);
-            pre=temp;
+        dp=new int[n+1][2];
+        for(int[] i:dp){
+            Arrays.fill(i,-1);
         }
-        return cur;
+        return Math.max(solve(nums, 0,1), solve(nums, 0,0));
+    }
+    private int solve(int[] nums, int i, int take){
+        if(i==nums.length) return 0;
+        if(dp[i][take]!=-1) return dp[i][take];
+        if(take==1) return dp[i][take]=solve(nums, i+1, 0);
+        return dp[i][take]=Math.max(nums[i]+solve(nums, i+1, 1), 
+                                solve(nums, i+1,0));
     }
 }
