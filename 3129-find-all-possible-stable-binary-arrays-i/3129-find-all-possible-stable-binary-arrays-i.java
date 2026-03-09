0ad64f2ccd35f3bd8f476c8c9aad092e9e23d855
Pushkar Shinde <pushkarshinde24@gmail.com>
2026-03-09 11:42:56 +0530

    Time: 259 ms (32.89%), Space: 46.7 MB (88.16%) - LeetHub

--- 3129-find-all-possible-stable-binary-arrays-i/3129-find-all-possible-stable-binary-arrays-i.java
+++ 3129-find-all-possible-stable-binary-arrays-i/3129-find-all-possible-stable-binary-arrays-i.java
@@ -1,7 +1,18 @@
 class Solution {
+    private int limit;
     private int mod=(int)1e9+7;
+    int[][][] dp;
     public int numberOfStableArrays(int zero, int one, int limit) {
-        long[][][] dp=new long[zero+1][one+1][2];
+        this.limit=limit;
+        dp=new int[zero+1][one+1][2];
+        for(int i=0;i<=zero;i++){
+            for(int j=0;j<=one;j++){
+                Arrays.fill(dp[i][j], -1);
+            }
+        }
+        /*
+        TABULATION 
+
         for(int k=0;k<=Math.min(zero,limit);k++){
             dp[k][0][0]=1;
         }
@@ -29,5 +40,26 @@ class Solution {
             }
         }
         return (int)((dp[zero][one][1]+dp[zero][one][0])%mod);
+        */
+        return (solve(zero, one, true)+solve(zero, one, false))%mod;
+    }
+
+    private int solve(int leftZero, int leftOne, boolean one){
+        if(leftZero==0 && leftOne==0) return 1;
+
+        if(dp[leftZero][leftOne][one?1:0]!=-1) return dp[leftZero][leftOne][one?1:0];
+
+        int res=0;
+        if(one){//iske pehle one liya tha
+            for(int i=1;i<=Math.min(leftZero,limit);i++){
+                res=(res+solve(leftZero-i, leftOne, false))%mod;
+            }
+        }
+        if(!one){//iske pehle one liya tha
+            for(int i=1;i<=Math.min(leftOne,limit);i++){
+                res=(res+solve(leftZero, leftOne-i, true))%mod;
+            }
+        }
+        return dp[leftZero][leftOne][one?1:0]=res%mod;
     }
 }
