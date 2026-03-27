29b8d44995d8a9263a9635bee43ef64b1d5e3a0f
Pushkar Shinde <pushkarshinde24@gmail.com>
2026-03-27 22:47:09 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Hard/Chocolates Pickup/chocolates-pickup.java
+++ Difficulty: Hard/Chocolates Pickup/chocolates-pickup.java
@@ -1,33 +1,48 @@
 class Solution {
-    int[][][] dp;
+    
     public int maxChocolate(int grid[][]) {
         int n=grid.length;
         int m=grid[0].length;
-        dp=new int[n][m][m];
-        for(int i=0;i<n;i++){
-            for(int j=0;j<m;j++){
-                Arrays.fill(dp[i][j],-1);
+        
+        int[][][] dp=new int[n][m][m];
+        
+        for(int j1=0;j1<m;j1++){
+            for(int j2=0;j2<m;j2++){
+                if(j1!=j2){
+                    dp[n-1][j1][j2]=grid[n-1][j1]+grid[n-1][j2];
+                }else{
+                    dp[n-1][j1][j2]=grid[n-1][j1];
+                }
             }
         }
-        return solve(grid,0,0,m-1);
-    }
-    private int solve(int[][] grid, int r, int c1, int c2){
-        int n=grid.length;
-        int m=grid[0].length;
-        if(r>=n) return 0;
-        if(dp[r][c1][c2]!=-1) return dp[r][c1][c2];
-        int cherry=grid[r][c1];
-        if(c1!=c2) cherry+=grid[r][c2];
-        int ans=0;
-        for(int i=-1;i<=1;i++){
-            for(int j=-1;j<=1;j++){
-                int x=r+1;
-                int y1=c1+i;
-                int y2=c2+j;
-                if(y1<0 || y2<0 || y1>=m || y2>=m) continue;
-                ans=Math.max(ans,solve(grid,x,y1,y2));
+        for(int i=n-2;i>=0;i--){
+            for(int j1=0;j1<m;j1++){
+                for(int j2=0;j2<m;j2++){
+                    int maxVal=(int)-1e8;
+                    for(int x=-1;x<=1;x++){
+                        for(int y=-1;y<=1;y++){
+                            int val=0;
+                            if(j1==j2){
+                                val=grid[i][j1];
+                            }else{
+                                val=grid[i][j1]+grid[i][j2];
+                            }
+                            if(j1+x<0 || j1+x>=m || j2+y<0 || j2+y>=m){
+                                val+=(int)-1e8;
+                            }else{
+                                val+=dp[i+1][j1+x][j2+y];;
+                            }
+                            maxVal=Math.max(maxVal, val);
+                        }
+                    }
+                    dp[i][j1][j2]=maxVal;
+                }
             }
         }
-        return dp[r][c1][c2]=ans+cherry;
+        
+        
+        return dp[0][0][m-1];
     }
+    
+
 }
