df84f644ef328c83d1124611b876a3c5790be9f5
Pushkar Shinde <pushkarshinde24@gmail.com>
2026-04-17 16:35:19 +0530

    Time: 81 ms (25.89%), Space: 132.4 MB (5.08%) - LeetHub

--- 3761-minimum-absolute-distance-between-mirror-pairs/3761-minimum-absolute-distance-between-mirror-pairs.java
+++ 3761-minimum-absolute-distance-between-mirror-pairs/3761-minimum-absolute-distance-between-mirror-pairs.java
@@ -1,24 +1,49 @@
 class Solution {
     public int minMirrorPairDistance(int[] nums) {
-        Map<Integer, Integer> map=new HashMap<>();
         int n=nums.length;
-        int res=Integer.MAX_VALUE;
+        Map<Integer, List<Integer>> map=new HashMap<>();
         for(int i=0;i<n;i++){
-            int newNum=reverse(nums[i]);
-            if(map.containsKey(nums[i])){
-                res=Math.min(i-map.get(nums[i]), res);
-            }
-            map.put(newNum, i);
+            int val=nums[i];
+            map.computeIfAbsent(val,k->new ArrayList<>()).add(i);
         }
-        return res==Integer.MAX_VALUE?-1:res;
+
+        int minDist=Integer.MAX_VALUE;
+
+        for(int i=0;i<n;i++){
+            int reverse=rev(nums[i]);
+            if(!map.containsKey(reverse)) continue;
+
+            List<Integer> list=map.get(reverse);
+            int ind=upper(list, i);
+            if(ind<list.size()){
+                minDist=Math.min(minDist, list.get(ind)-i);
+            }
+        }
+
+        return minDist==Integer.MAX_VALUE?-1:minDist;
     }
-    private int reverse(int num){
+
+    private int upper(List<Integer> list, int tar){
+        int l=0, r=list.size();
+        int ans=0;
+        while(l<r){
+            int mid=l+(r-l)/2;
+            if(list.get(mid)<=tar){
+                l=mid+1;
+            }else{
+                r=mid;
+            }
+        }
+        return l;
+    }
+    
+    private int rev(int num){
         int res=0;
         while(num>0){
-            int rem=num%10;
-            res=res*10+rem;
+            res=res*10+num%10;
             num/=10;
         }
+        
         return res;
     }
 }
