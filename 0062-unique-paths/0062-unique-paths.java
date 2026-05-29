1aec743f0954efd48803beb6bd1adf3b2fe4d582
Pushkar Shinde <pushkarshinde24@gmail.com>
2026-05-30 00:32:07 +0530

    Time: 0 ms (100%), Space: 42.2 MB (46.17%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.java
+++ 0062-unique-paths/0062-unique-paths.java
@@ -1,24 +1,42 @@
 class Solution {
-    private int[][] dp;
+    // private long mod=(long)1e9+7;
+    // private long[] fact;
+    // private long[] reverse;
+    // private long power(long a, long b){
+    //     long res=1;
+    //     while(b>0){
+    //         if((b&1)==1) res=(res*a)%mod;
+    //         a=(a*a)%mod;
+    //         b>>=1;
+    //     }
+    //     return res;
+    // }
     public int uniquePaths(int m, int n) {
-        dp=new int[m][n];
-        for(int i=0;i<m;i++){
-            Arrays.fill(dp[i],-1);
+        // int size=n+m+5;
+        // reverse=new long[size];
+        // fact=new long[size];
+
+        // fact[0]=1;
+        // for(int i=1;i<size;i++){
+        //     fact[i]=(fact[i-1]*i)%mod;
+        // }
+
+        // reverse[size-1]=power(fact[size-1],mod-2);
+        // for(int i=size-2;i>=0;i--){
+        //     reverse[i]=(reverse[i+1]*(i+1))%mod;
+        // }
+
+        // int total=n+m-2;
+        // long res=((fact[total]*reverse[m-1])%mod*reverse[n-1])%mod;
+        // return (int)res;
+
+        long res=1;
+        int size=n+m-2;
+        int r=Math.min(n-1,m-1);
+        for(int i=1;i<=r;i++){
+            res=res*(size-r+i)/i;
         }
-        return solve(m,n, 0,0);
-    }
-    int[][] dir={{0,1},{1,0}};
-    private int solve(int m, int n, int i, int j){
-        if(i==m-1 && j==n-1){
-            return 1;
-        }
-        if(dp[i][j]!=-1) return dp[i][j];
-        int ways=0;
-        for(int[] d:dir){
-            int nx=i+d[0], ny=j+d[1];
-            if(nx<0 || ny<0 || nx>=m || ny>=n) continue;
-            ways+=solve(m,n,nx,ny);
-        }
-        return dp[i][j]=ways;
+
+        return (int)res;
     }
 }
