dc9025416fbf3d4c08b9cc3f26943790b22a9424
Pushkar Shinde <pushkarshinde24@gmail.com>
2026-06-16 14:12:30 +0530

    Time: 4 ms (76.47%), Space: 55.1 MB (75%) - LeetHub

---
+++ 3612-process-string-with-special-operations-i/3612-process-string-with-special-operations-i.java
@@ -0,0 +1,17 @@
+class Solution {
+    public String processStr(String s) {
+        StringBuilder res=new StringBuilder();
+        for(char c:s.toCharArray()){
+            if(c=='*'){
+                if(!res.isEmpty()) res.deleteCharAt(res.length()-1);
+            }else if(c=='%'){
+                res.reverse();
+            }else if(c=='#'){
+                res.append(res);
+            }else {
+                res.append(c);
+            }
+        }
+        return res.toString();
+    }
+}
