9e3347eb81fe44dc71200dde2b32ae665f8de8f9
Pushkar Shinde <pushkarshinde24@gmail.com>
2026-07-20 00:06:47 +0530

    Time: 3 ms (2.2%), Space: 44.1 MB (15.01%) - LeetHub

--- 0199-binary-tree-right-side-view/0199-binary-tree-right-side-view.java
+++ 0199-binary-tree-right-side-view/0199-binary-tree-right-side-view.java
@@ -13,21 +13,43 @@
  *     }
  * }
  */
+
+class Node{
+    int level;
+    TreeNode node;
+    Node(int l, TreeNode n){
+        this.level=l;
+        this.node=n;
+    }
+}
 class Solution {
     public List<Integer> rightSideView(TreeNode root) {
+        Map<Integer, TreeNode> map=new TreeMap<>();
         List<Integer> res=new ArrayList<>();
         if(root==null) return res;
-        Queue<TreeNode> nodes=new LinkedList<>();
-        nodes.offer(root);
-        while(!nodes.isEmpty()){
-            int n=nodes.size();
-            for(int i=0;i<n;i++){
-                TreeNode node=nodes.poll();
-                if(i==n-1) res.add(node.val);
-                if(node.left!=null)nodes.offer(node.left);
-                if(node.right!=null)nodes.offer(node.right);
+
+        Queue<Node> q=new ArrayDeque<>();
+        q.offer(new Node(0,root));
+        while(!q.isEmpty()){
+            Node cur=q.poll();
+            TreeNode parent=cur.node;
+            int level=cur.level;
+
+            map.put(level, parent);
+
+            if(parent.left!=null){
+                q.offer(new Node(level+1, parent.left));
+            }
+            if(parent.right!=null){
+                q.offer(new Node(level+1, parent.right));
             }
         }
+
+        for(int key: map.keySet()){
+            TreeNode node=map.get(key);
+            res.add(node.val);
+        }
+
         return res;
-    } 
+    }
 }
