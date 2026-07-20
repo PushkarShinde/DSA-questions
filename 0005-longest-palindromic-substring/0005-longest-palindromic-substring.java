7a5484491ff00d3c09259f6347c75a4729cf4900
Pushkar Shinde <pushkarshinde24@gmail.com>
2026-07-20 14:49:43 +0530

    Time: 176 ms (21.71%), Space: 48.3 MB (7.21%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.java
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.java
@@ -2,38 +2,59 @@ class Solution {
     int[][] dp;
     public String longestPalindrome(String s) {
         int n=s.length();
-        int len=0;
+        int length=0;
         int start=0;
         
-        dp=new int[n][n];
-        for(int[] d:dp){
-            Arrays.fill(d,-1);
-        }
+        // dp=new int[n][n];
+        // for(int[] d:dp){
+        //     Arrays.fill(d,-1);
+        // }
 
-        for(int i=0;i<n;i++){
-            for(int j=i;j<n;j++){
-                if(solve(s, i,j)){
-                    if(len<j-i+1){
-                        len=j-i+1;
-                        start=i;
-                    }
+        // for(int i=0;i<n;i++){
+        //     for(int j=i;j<n;j++){
+        //         if(solve(s, i,j)){
+        //             if(len<j-i+1){
+        //                 len=j-i+1;
+        //                 start=i;
+        //             }
+        //         }
+        //     }
+        // }
+        
+        int res=0;
+        boolean[][] dp=new boolean[n][n];
+        for(int len=1;len<=n;len++){
+            for(int i=0;i+len-1<n;i++){
+                int j=i+len-1;
+
+                if(i==j){
+                    dp[i][j]=true;
+                }else if(i+1==j){
+                    dp[i][j]=(s.charAt(i)==s.charAt(j));
+                }else{
+                    dp[i][j]=s.charAt(i)==s.charAt(j) && dp[i+1][j-1];
+                }
+
+                if(dp[i][j] && length<j-i+1){
+                    length=j-i+1;
+                    start=i;
                 }
             }
         }
 
-        return s.substring(start, start+len);
+        return s.substring(start, start+length);
     }
 
-    private boolean solve(String s, int i, int j){
-        if(i>=j) return true;
-        if(dp[i][j]!=-1) return dp[i][j]==1;
+    // private boolean solve(String s, int i, int j){
+    //     if(i>=j) return true;
+    //     if(dp[i][j]!=-1) return dp[i][j]==1;
 
-        boolean res=false;
-        if(s.charAt(i)==s.charAt(j)){
-            res |=solve(s, i+1, j-1);
-        }
+    //     boolean res=false;
+    //     if(s.charAt(i)==s.charAt(j)){
+    //         res |=solve(s, i+1, j-1);
+    //     }
 
-        dp[i][j]=(res?1:0);
-        return res;
-    }
+    //     dp[i][j]=(res?1:0);
+    //     return res;
+    // }
 }
